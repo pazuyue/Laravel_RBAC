7b890ca20c0e9fef200f8fe0d0b35e75a569fcc6
yueguang <yueguang@qingmutec.com>
2018-06-28 20:05:53 +0800

    调整

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 
-@section('title', '| Create New Post')
+
 
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
 
-            <h1>Create New Post</h1>
-            <hr>
+                <h1>Create New Post</h1>
+                <hr>
 
-            {{-- 使用 Laravel HTML Form Collective 创建表单 --}}
-            {{ Form::open(array('route' => 'posts.store')) }}
+                {{-- 使用 Laravel HTML Form Collective 创建表单 --}}
+                {{ Form::open(array('route' => 'posts.store')) }}
 
-            <div class="form-group">
-                {{ Form::label('title', 'Title') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
-                <br>
+                <div class="form-group">
+                    {{ Form::label('title', 'Title') }}
+                    {{ Form::text('title', null, array('class' => 'form-control')) }}
+                    <br>
 
-                {{ Form::label('body', 'Post Body') }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
-                <br>
+                    {{ Form::label('body', 'Post Body') }}
+                    {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                    <br>
 
-                {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block')) }}
-                {{ Form::close() }}
+                    {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block')) }}
+                    {{ Form::close() }}
+                </div>
             </div>
         </div>
     </div>
-
 @endsection
